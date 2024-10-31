12aa77aa91eee85111e5f158180e6dba9762add5
Flc <four_leaf_clover@foxmail.com>
2024-10-31 18:40:18 +0800

    build(split): add support for repo sub-packages

---
+++ bin/split-linux.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+BASEPATH=$(cd `dirname $0`; cd ../src/; pwd)
+REPOS=$@
+
+function split()
+{
+    SHA1=`./bin/splitsh-lite-linux --prefix=$1`
+    git push $2 "$SHA1:refs/heads/$CURRENT_BRANCH" -f
+}
+
+function remote()
+{
+    git remote add $1 $2 || true
+}
+
+git pull origin $CURRENT_BRANCH
+
+if [[ $# -eq 0 ]]; then
+    REPOS=$(ls $BASEPATH)
+fi
+
+remote github git@github.com:hyperf-contrib/.github.git
+split "src/.github" "git@github.com:hyperf-contrib/.github.git"
+
+for REPO in $REPOS ; do
+    remote $REPO git@github.com:hyperf-contrib/$REPO.git
+
+    split "src/$REPO" $REPO
+done
