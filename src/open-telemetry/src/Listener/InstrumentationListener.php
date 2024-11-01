f40d43b84fe2c59b324ea96b3b92de7537307353
Flc <four_leaf_clover@foxmail.com>
2024-11-01 13:59:11 +0800

    🛠️refactor(open-telemetry) centralize exception recording in InstrumentationListener

--- src/open-telemetry/src/Listener/InstrumentationListener.php
+++ src/open-telemetry/src/Listener/InstrumentationListener.php
@@ -6,12 +6,9 @@ namespace HyperfContrib\OpenTelemetry\Listener;
 
 use Hyperf\Contract\ConfigInterface;
 use Hyperf\Contract\ContainerInterface;
+use HyperfContrib\OpenTelemetry\Concerns\SpanRecordThrowable;
 use HyperfContrib\OpenTelemetry\Switcher;
 use OpenTelemetry\API\Instrumentation\CachedInstrumentation;
-use OpenTelemetry\API\Trace\SpanInterface;
-use OpenTelemetry\API\Trace\StatusCode;
-use OpenTelemetry\SemConv\TraceAttributes;
-use Throwable;
 
 /**
  * Class InstrumentationListener.
@@ -24,6 +21,8 @@ use Throwable;
  */
 abstract class InstrumentationListener
 {
+    use SpanRecordThrowable;
+
     protected readonly ConfigInterface $config;
 
     /**
@@ -38,25 +37,5 @@ abstract class InstrumentationListener
         protected readonly Switcher $switcher,
     ) {
         $this->config = $this->container->get(ConfigInterface::class);
-    }
-
-    /**
-     * Record exception to span.
-     *
-     * @param SpanInterface $span
-     * @param Throwable $e
-     * @return void
-     */
-    protected function spanRecordException(SpanInterface $span, Throwable $e): void
-    {
-        $span->setAttributes([
-            TraceAttributes::EXCEPTION_TYPE       => get_class($e),
-            TraceAttributes::EXCEPTION_MESSAGE    => $e->getMessage(),
-            TraceAttributes::EXCEPTION_STACKTRACE => $e->getTraceAsString(),
-            TraceAttributes::CODE_FUNCTION        => $e->getFile() . ':' . $e->getLine(),
-            TraceAttributes::CODE_LINENO          => $e->getLine(),
-        ]);
-        $span->setStatus(StatusCode::STATUS_ERROR, $e->getMessage());
-        $span->recordException($e);
     }
 }
